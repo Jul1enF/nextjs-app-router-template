f31eaae76bde741fc10f0348c3ab59773d2cace3
Jul1enF <j.furic@gmail.com>
2025-12-10 19:24:53 +0100

    confirmation modal added

--- components/pages/index/Home.js
+++ components/pages/index/Home.js
@@ -1,14 +1,23 @@
 'use client'
 
 import styles from '@/styles/pages/home/Home.module.css';
+import ConfirmationModal from '@/components/ui/ConfirmationModal';
+import { useState } from 'react';
 
 function Home() {
+  const [modalVisible, setModalVisible]=useState(false)
 
   return (
     <div className="pageBody">
         <h2 className="pageTitle">
           What's next ?!
         </h2>
+
+        <button className='regularItem strongRedBg regularText' onClick={()=>setModalVisible(true)}>
+          Confirmation
+        </button>
+
+        <ConfirmationModal visible={modalVisible} confirmationText={"Êtes vous sûr de vouloir gagner 1 million d'euros ?"} confirmationButtonText={"Oui je suis sûr"} cancelButtonText={"Non je préfère un chocolat"} confirmationFunction={()=> setModalVisible(false)} closeModal={() => setModalVisible(false)}/>
     </div>
   );
 }
