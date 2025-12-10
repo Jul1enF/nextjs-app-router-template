033226961a79eb650cdb8977f60278f60ce0c07c
Jul1enF <j.furic@gmail.com>
2025-12-10 15:03:39 +0100

    TabBar added

--- components/layout/AppLayoutWrapper.js
+++ components/layout/AppLayoutWrapper.js
@@ -1,14 +1,20 @@
 'use client'
 
 import Header from "@/components/layout/Header"
+import PhoneTabBar from "@/components/layout/phone/PhoneTabBar";
+import useWindowDimensions from '@/hooks/useWindowDimensions';
 
 
 export default function AppLayoutWrapper({ children }) {
+    const { computerDisplay } = useWindowDimensions()
+
     return (
         <>
             <Header />
 
             {children}
+
+            {!computerDisplay && <PhoneTabBar />}
         </>
     )
 
